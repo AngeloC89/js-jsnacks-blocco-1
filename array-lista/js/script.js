b47a1acd471edcf964250aab9953ba342244d8a7
AngeloC89 <Angelociulla89@gmail.com>
2024-03-12 15:07:31 +0100

    created array fruit

---
+++ array-lista/js/script.js
@@ -0,0 +1,24 @@
+// 1. creare l'array con la frutta del frigorifero
+// 2. aggiungere la pesca all'array della frutta
+// 3. verificare se nell'array di frutta c'è il cocomero:
+//    - se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
+//    - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
+// Suggerimenti/Indicazioni:
+// Sì, lo sappiamo che esiste la funzione includes() ma noi non la vogliamo utilizzare per cercare nel frigorifero.
+
+let frigorifero = [
+    'banana',
+    'mela',
+    'pera',
+    'ciliegia',
+    'arancia',
+    'mandarino',
+    'cocomero',
+    'limone',
+    'fragola'
+];
+
+
+console.log(frigorifero)
+
+frigorifero.push('pesca')
