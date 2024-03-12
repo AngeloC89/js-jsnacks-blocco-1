dec09569a385eafdff6be626f7bacb9f1aba2741
AngeloC89 <Angelociulla89@gmail.com>
2024-03-12 16:53:30 +0100

    array bonus work

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,8 +1,6 @@
 //Crea due array che hanno un numero di elementi diversi.
 // Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 
-
-
 let longArray = [1,2,3,4,5,6,7,8,9,10];
 
 let shortArray = [1,2,3,4,5];
@@ -10,3 +8,9 @@ let shortArray = [1,2,3,4,5];
 let aDiff = longArray.length - shortArray.length;
 console.log(aDiff);
 
+for(i = 0; i < aDiff; i++){
+    shortArray.push(getRndInteger(1, 100));
+    console.log(shortArray);
+
+
+};
